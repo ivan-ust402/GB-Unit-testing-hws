9d6d99b8153349520fc496e5461438593eaffa0d
Ivan Ustyantsev <vangold915@gmail.com>
2023-12-12 16:28:34 +0700

    Some bugs fixed

--- homework1/src/main/java/homework1/Calculator.java
+++ homework1/src/main/java/homework1/Calculator.java
@@ -1,5 +1,9 @@
 package homework1;
 
+// Задание 1. ** В классе Calculator создайте метод calculateDiscount, который принимает сумму покупки и процент скидки
+// и возвращает сумму с учетом скидки. Ваша задача - проверить этот метод с использованием библиотеки AssertJ. Если
+// метод calculateDiscount получает недопустимые аргументы, он должен выбрасывать исключение ArithmeticException.
+// Не забудьте написать тесты для проверки этого поведения.
 public class Calculator {
     public double calculateDiscount(double amount, double discount) {
         if (amount < 0 || discount < 0 || discount > 100) {
