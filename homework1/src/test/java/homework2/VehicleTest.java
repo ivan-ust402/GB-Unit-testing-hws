9d2e76d1a0af7ae9eef01c94d51e47222e37e419
Ivan Ustyantsev <vangold915@gmail.com>
2023-12-16 17:07:59 +0700

    homework2: initial commit

---
+++ homework1/src/test/java/homework2/VehicleTest.java
@@ -0,0 +1,16 @@
+package homework2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTest {
+
+    @Test
+    void testDrive() {
+    }
+
+    @Test
+    void park() {
+    }
+}
